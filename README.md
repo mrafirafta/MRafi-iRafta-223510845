e28893f38b91caff220239d5bf21dbdd8c089913
rafi <rafiaja132018@gmail.com>
2024-04-06 22:25:34 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Vue Tailwind Portfolio
+
+Fancy and minimal portfolio made on VUE JS and Tailwind CSS.
+
+## Features
+
+- Light/dark mode toggle
+- Fetched Projects from github repositories
+- Responsive
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/Bagus324/vuejs-tailwind-simple-portofolio.git
+```
+
+Go to the project directory
+
+```bash
+  cd vuejs-tailwind-simple-portfolio
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run dev
+```
+
+## 🔗 Links
+
+
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](#)
+
+## Demo
+
+Link: [Demo](https://vuejs-tailwind-simple-portofolio.vercel.app/)
---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="text-center">
+        <h2 class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white">Hi! I'm Muhammad Rafi'i Rafta </h2>
+       <p style="left: 0%;" class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300">
+    <center> > Saya adalah Mahasiswa Universitas Islam Riau yang sekarang adalah mahasiswa semester 4, selain menjadi mahasiswa saya juga bekerja part-time di Sekarsa sebagai barista.</center>
+           <br>
+        <center>  > Saya memiliki skill yang dimana didalam pengeditan foto atau membikin sebuah flyer</center>
+           <br>
+        <center> > Saya juga salah satu staff didalam organisasi HIMATIF di KOMINFO</center>
+        </p>
+        <!-- <p class="text-center text-blue-600">#DotNetCore #WebApplication</p> -->
+    </div>
+</template>
+<script setup>
+
+
+</script>
